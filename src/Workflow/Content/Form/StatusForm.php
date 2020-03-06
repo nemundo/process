3861d6343af081aed812a558175706250025fba1
nemundo <urs.lang@gmail.com>
2020-03-06 13:27:51 +0100

    .

--- src/Workflow/Content/Form/StatusForm.php
+++ src/Workflow/Content/Form/StatusForm.php
@@ -5,6 +5,7 @@ namespace Nemundo\Process\Workflow\Content\Form;
 
 
 
+use Nemundo\Admin\Com\Title\AdminSubtitle;
 use Nemundo\Core\Language\Translation;
 use Nemundo\Html\Paragraph\Paragraph;
 
@@ -17,8 +18,11 @@ class StatusForm extends AbstractStatusForm
     public function getContent()
     {
 
-        $p = new Paragraph($this);
-        $p->content = 'Process Status: ' .(new Translation())->getText( $this->contentType->typeLabel);
+        //$p = new Paragraph($this);
+        //$p->content = 'Process Status: ' .(new Translation())->getText( $this->contentType->typeLabel);
+
+        $subtitle=new AdminSubtitle($this);
+        $subtitle->content= (new Translation())->getText( $this->contentType->typeLabel);
 
         return parent::getContent();
     }
