33965da244511b085f2d3266f48913e2371befe6
nemundo <urs.lang@gmail.com>
2020-03-17 18:57:07 +0100

    .

--- src/App/_Inbox/Data/InboxCollection.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Nemundo\Process\App\Inbox\Data;
-use Nemundo\Model\Collection\AbstractModelCollection;
-class InboxCollection extends AbstractModelCollection {
-protected function loadCollection() {
-$this->addModel(new \Nemundo\Process\App\Inbox\Data\Inbox\InboxModel());
-}
-}
